ce9a0f4329a54815fa87d107efc4e484b961dc6b
Pratham verma <pratham.verma@1mg.com>
2025-03-10 23:29:26 +0530

    auth added

--- src/clients/BrowserClient.ts
+++ src/clients/BrowserClient.ts
@@ -4,7 +4,7 @@ export class BrowserClient {
     public async initiateExtensionLogin(supabaseSessionId: string) {
         const isExternalAuthRequest = "DeputyDev Extension"
         console.log("opening browser");
-        const authUrl = `http://localhost:3000/external-auth?supabase_session_id=${supabaseSessionId}&is_external_auth_request=${isExternalAuthRequest}`;
+        const authUrl = `https://stag.deputydev.ai/external-auth?supabase_session_id=${supabaseSessionId}&is_external_auth_request=${isExternalAuthRequest}`;
         await vscode.env.openExternal(vscode.Uri.parse(authUrl));
     }
 }
