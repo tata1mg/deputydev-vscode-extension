c59aaf7f9d2e321cf3de829477b47890a99c8d99
cons-vikrant-chauhan@1mg.com <vik.chauhan001@gmail.com>
2025-03-11 13:55:34 +0000

    Use common config for endpoints

--- src/services/references/ReferenceService.ts
+++ src/services/references/ReferenceService.ts
@@ -1,44 +1,28 @@
-import { api } from "../api/axios";
+import { binaryApi } from "../api/axios";
 import { API_ENDPOINTS } from "../api/endpoints";
 
-
 export class ReferenceService {
-    public async keywordSearch(payload: unknown): Promise<any> {
-        let response;
-        try {
-            response = await api({
-                // url: API_ENDPOINTS.KEYWORD_SEARCH, TODO: Change this later
-                url: 'http://localhost:9000/v1/keyword_search',
-                method: 'post',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                },
-                data: payload,
-            });
-            return response.data;
-        } catch (error) {
-            console.error('Error in getReference API call:', error);
-            throw error;
-        }
+  public async keywordSearch(payload: unknown): Promise<any> {
+    let response;
+    try {
+      response = await binaryApi.post(API_ENDPOINTS.KEYWORD_SEARCH, payload);
+      return response.data;
+    } catch (error) {
+      console.error("Error in getReference API call:", error);
+      throw error;
     }
-    public async keywordTypeSearch(payload: unknown): Promise<any> {
-        let response;
-        try {
-            response = await api({
-                // url: API_ENDPOINTS.KEYWORD_TYPE_SEARCH, TODO: Change this later
-                url: 'http://localhost:9000/v1/keyword_type_search',
-                method: 'post',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                },
-                data: payload,
-            });
-            return response.data;
-        } catch (error) {
-            console.error('Error in getReference API call:', error);
-            throw error;
-        }
+  }
+  public async keywordTypeSearch(payload: unknown): Promise<any> {
+    let response;
+    try {
+      response = await binaryApi.post(
+        API_ENDPOINTS.KEYWORD_TYPE_SEARCH,
+        payload
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error in getReference API call:", error);
+      throw error;
     }
+  }
 }
