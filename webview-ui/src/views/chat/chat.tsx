b91ba0fa04e20cff8fdd45dadd1cce0a9161022d
Pratham verma <pratham.verma@1mg.com>
2025-03-13 15:56:00 +0530

    common parser

--- webview-ui/src/views/chat/chat.tsx
+++ webview-ui/src/views/chat/chat.tsx
@@ -40,7 +40,7 @@ export function ChatUI() {
     showSessionsBox,
     showAllSessions,
     sessions,
-    sessionChats,
+    // sessionChats,
     ChatAutocompleteOptions,
   } = useChatStore();
   const { chatType, setChatType } = useChatSettingStore();
@@ -198,13 +198,13 @@ export function ChatUI() {
     if (chatContainerEndRef.current) {
       chatContainerEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  }, [sessionChats]);
+  }, [messages]);
 
   return (
     <div className="flex flex-col justify-between h-full relative">
       <div className="flex-grow overflow-y-auto">
         {/* Past Sessions */}
-        {showSessionsBox && sessionChats.length === 0 && (
+        {showSessionsBox && messages.length === 0 && (
           <div>
             <div className="mb-14 mt-10">
               <BotMessageSquare className="px-4 h-20 w-20 text-white" />
@@ -289,10 +289,10 @@ export function ChatUI() {
           </div>
         )}
 
-        {sessionChats.length > 0 && <ParserUI sessionChats={sessionChats} />}
+        {/* {messages.length > 0 && <ChatArea/>} */}
 
         {/* Invisible div just to instant scroll to bottom for session chats */}
-        <div ref={chatContainerEndRef} />
+        {/* <div ref={chatContainerEndRef} /> */}
 
         <div className="flex-grow space-y-4 py-2 overflow-auto">
           <ChatArea />
