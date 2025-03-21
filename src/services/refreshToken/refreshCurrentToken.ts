43905a1bee3b4d63f29fc54912a4bee0e8d16c4f
Pratham verma <pratham.verma@1mg.com>
2025-03-21 14:29:13 +0530

    fix in refresh token

--- src/services/refreshToken/refreshCurrentToken.ts
+++ src/services/refreshToken/refreshCurrentToken.ts
@@ -1,10 +1,19 @@
-import { AxiosResponseHeaders } from "axios";
+import { AuthService } from "../auth/AuthService";
 
-export function refreshCurrentToken(responseHeaders: any) {
+export async function refreshCurrentToken(responseHeaders: any) {
     if (responseHeaders) {
         const refreshedAuthToken = responseHeaders["new_session_data"];
         if (refreshedAuthToken) {
             console.log("refreshed auth token", refreshedAuthToken)
+            try {
+                const authService = new AuthService();
+                const result = await authService.storeAuthToken(refreshedAuthToken);
+                if (result === "success") {
+                    return;
+                }
+            } catch (error) {
+                console.error("Error while updating refreshed Token: ", error)
+            }
         }
     }
 }
