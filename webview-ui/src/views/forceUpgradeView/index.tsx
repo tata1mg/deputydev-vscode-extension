692c401af25ae5775516d1cdd62425a92f85d2a1
Pratham verma <pratham.verma@1mg.com>
2025-03-24 13:28:37 +0530

    force upgrade view added

---
+++ webview-ui/src/views/forceUpgradeView/index.tsx
@@ -0,0 +1,10 @@
+export default function ForceUpgradeView() {
+  return (
+      <div className="flex flex-col items-center justify-center h-screen p-4">
+          <h1 className="text-xl font-bold mb-4">Update Required</h1>
+          <p className="text-center mb-4 text-gray-500">
+              You need to install the updated version of the DeputyDev extension to continue using the application.
+          </p>
+      </div>
+  );
+}
