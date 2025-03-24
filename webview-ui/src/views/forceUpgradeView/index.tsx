02aa52d978e4d45c0646c8690ca4965db6a16429
Pratham verma <pratham.verma@1mg.com>
2025-03-24 13:39:48 +0530

    minor change

--- webview-ui/src/views/forceUpgradeView/index.tsx
+++ webview-ui/src/views/forceUpgradeView/index.tsx
@@ -3,7 +3,7 @@ export default function ForceUpgradeView() {
       <div className="flex flex-col items-center justify-center h-screen p-4">
           <h1 className="text-xl font-bold mb-4">Update Required</h1>
           <p className="text-center mb-4 text-gray-500">
-              You need to install the updated version of the DeputyDev extension to continue using the application.
+              You need to install the updated version of the DeputyDev to continue using the application.
           </p>
       </div>
   );
