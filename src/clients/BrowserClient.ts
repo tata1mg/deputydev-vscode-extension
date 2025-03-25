2688d63e266e948b3f199ab61ca39660b319ae7d
Pratham verma <pratham.verma@1mg.com>
2025-03-25 18:15:14 +0530

    unused line remove

--- src/clients/BrowserClient.ts
+++ src/clients/BrowserClient.ts
@@ -3,9 +3,8 @@ import {  getEssentialConfig, CLIENT } from '../config';
 
 export class BrowserClient {
     public async initiateExtensionLogin(supabaseSessionId: string) {
-        const isExternalAuthRequest = "DeputyDev Extension"
         console.log("opening browser");
-        const client_url = getEssentialConfig()["DD_BROWSER_HOST"];  
+        const client_url = getEssentialConfig()["DD_BROWSER_HOST"];
         const authUrl = `${client_url}/external-auth?supabase_session_id=${supabaseSessionId}&is_external_auth_request=${CLIENT}`;
         await vscode.env.openExternal(vscode.Uri.parse(authUrl));
     }
