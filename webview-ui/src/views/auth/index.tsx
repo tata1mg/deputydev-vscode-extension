9ca252e0889017dca8a933bf9221ba80b0b04f9f
Pratham verma <pratham.verma@1mg.com>
2025-04-10 13:28:48 +0530

    new logo added

--- webview-ui/src/views/auth/index.tsx
+++ webview-ui/src/views/auth/index.tsx
@@ -10,7 +10,12 @@ export default function Auth() {
     return (
         <>
             <div className="mx-auto flex max-w-[300px] w-full flex-col justify-between items-center p-6 mt-[190px] mb-[220px]">
-                <span className="text-3xl mb-[25px] text-center">Develop with DeputyDev</span>
+                <img
+                    src="https://onemg.gumlet.io/dd_logo_with_name_10_04.png"
+                    alt="DeputyDev Logo"
+                    className="h-10 w-auto mb-[25px]"
+                />
+                {/* <span className="text-3xl mb-[25px] text-center">Develop with DeputyDev</span> */}
                 <span className="text-lg text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500 animate-gradient mb-[40px]">
                     Your AI sidekick that amplifies your impact—turning coding hours into minutes
                 </span>
