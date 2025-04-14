8af568f8bcd222651f62d28b82a9135cc71dc82d
Satyam Kumar <satyam.kumar1@1mg.com>
2025-04-14 21:00:39 +0530

    DD-432 - Fix logic for focused snippet searcher

--- src/config.ts
+++ src/config.ts
@@ -23,8 +23,8 @@ export function setBinaryPort(port: number) {
 }
 
 export function getBinaryPort(): number | null {
-  return 8001;
-  // return BINARY_PORT;
+  // return 8001;
+  return BINARY_PORT;
 }
 
 
