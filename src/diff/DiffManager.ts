f3e57a594bb329d7648c6d631989c82f5141fe43
Satyam Kumar <satyam.kumar1@1mg.com>
2025-04-24 12:53:56 +0530

    Add basic implementation of applyDiff

--- src/diff/DiffManager.ts
+++ src/diff/DiffManager.ts
@@ -1,6 +1,8 @@
 import * as vscode from "vscode";
 import { FileChangeStateManager } from "./fileChangeStateManager/fileChangeStateManager";
 import { DeputydevChangeProposer } from "./viewers/deputydevChangeProposer/deputydevChangeProposer";
+import { API_ENDPOINTS } from "../services/api/endpoints";
+import { binaryApi } from "../services/api/axios";
 
 
 export class DiffManager {
@@ -9,6 +11,7 @@ export class DiffManager {
   private outputChannel: vscode.LogOutputChannel;
 
   private deputydevChangeProposer: DeputydevChangeProposer | undefined;
+  private fileChangeStateManager: FileChangeStateManager | undefined;
 
   constructor(vscodeContext: vscode.ExtensionContext, changeStateStorePath: string, outputChannel: vscode.LogOutputChannel) {
     this.changeStateStorePath = changeStateStorePath;
@@ -18,7 +21,7 @@ export class DiffManager {
 
   // initialize the diff manager
   public init = async () => {
-    let fileChangeStateManager = new FileChangeStateManager(
+    this.fileChangeStateManager = new FileChangeStateManager(
       this.vscodeContext,
       this.outputChannel,
     );
@@ -26,8 +29,61 @@ export class DiffManager {
     this.deputydevChangeProposer = new DeputydevChangeProposer(
       this.vscodeContext,
       this.outputChannel,
-      fileChangeStateManager,
+      this.fileChangeStateManager,
     );
     await this.deputydevChangeProposer.init();
   }
+
+  private checkInit = () => {
+    if (!this.deputydevChangeProposer) {
+      throw new Error("DeputydevChangeProposer not initialized");
+    }
+    if (!this.fileChangeStateManager) {
+      throw new Error("FileChangeStateManager not initialized");
+    }
+  }
+
+  public applyDiff = async (data: { path: string; incrementalUdiff: string }, repoPath: string) => {
+    this.checkInit();
+    // first get the current content of the file to apply the diff on
+    const originalContent = await this.fileChangeStateManager.getOriginalContentToShowDiffOn(
+      data.path
+    );
+
+    // then apply the diff to the original content
+    let newContent = originalContent;
+    try {
+      const response = await binaryApi().post(API_ENDPOINTS.DIFF_APPLIER, {
+        repo_path: repoPath,
+        file_path_to_diff_map: {
+          [data.path]: data.incrementalUdiff,
+        },
+      }, { headers });
+      if (response.status !== 200) {
+        throw new Error(`Error applying diff: ${response.statusText}`);
+      }
+      newContent = response.data.file_path_to_diff_map[data.path];
+    } catch (error) {
+      this.outputChannel.error(`Error applying diff: ${error}`);
+      throw error;
+    }
+
+    // update the fileChangeStateMap with the original and modified content from the udiff
+    this.fileChangeStateManager?.updateFileStateInFileChangeStateMap(
+      data.path,
+      newContent,
+      originalContent
+    );
+
+  }
+
+
+  public openDiffView = async (data: { path: string; content: string }) => {
+    if (!this.deputydevChangeProposer) {
+      throw new Error("DiffManager not initialized");
+    }
+    await this.deputydevChangeProposer.openDiffView(data);
+  }
+  public acceptAllFiles = async () => { }
+  public rejectAllFiles = async () => { }
 }
