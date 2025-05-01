39247ee83bc4022c15c0a481f95a3953ce6a2901
Satyam Kumar <satyam.kumar1@1mg.com>
2025-05-01 16:12:59 +0530

    Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import prettierPlugin from "eslint-plugin-prettier"; // ✅ Import the plugin
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["webview-ui/**/*"]), {
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        prettier: prettierPlugin // ✅ Add the plugin
+    },
+
+    languageOptions: {
+        parser: tsParser,
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+                ignoreRestSiblings: true,
+            },
+        ],
+    },
+}]);
--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,27 +1,26 @@
 import axios from 'axios';
-import { DD_HOST,getBinaryHost ,CLIENT_VERSION , CLIENT } from '../../config';
+import { DD_HOST, getBinaryHost, CLIENT_VERSION, CLIENT } from '../../config';
 
 const api = axios.create({
-    baseURL: DD_HOST,
-    headers: {
-        'Content-Type': 'application/json',
-        'X-Client': CLIENT,
-        'X-Client-Version': CLIENT_VERSION,
-    },
-    withCredentials: false
+  baseURL: DD_HOST,
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Client': CLIENT,
+    'X-Client-Version': CLIENT_VERSION,
+  },
+  withCredentials: false,
 });
 
 export function binaryApi() {
-    return axios.create({
-      baseURL: getBinaryHost(),
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Client': CLIENT,
-        'X-Client-Version': CLIENT_VERSION,
-      },
-      withCredentials: false
-    });
-  }
-
+  return axios.create({
+    baseURL: getBinaryHost(),
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Client': CLIENT,
+      'X-Client-Version': CLIENT_VERSION,
+    },
+    withCredentials: false,
+  });
+}
 
 export { api };
