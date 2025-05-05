655ac222e13ed3afbacd458a16668e1c7b181bec
Satyam Kumar <satyam.kumar1@1mg.com>
2025-05-05 09:22:02 +0530

    Enable communication mechanism, content loading, inline buttons

--- src/diff/viewers/deputydevChangeProposer/editor/changeProposerEditor.ts
+++ src/diff/viewers/deputydevChangeProposer/editor/changeProposerEditor.ts
@@ -64,7 +64,7 @@ export class ChangeProposerEditor implements vscode.CustomEditorProvider<ChangeP
           });
           break;
         }
-        default : {
+        default: {
           console.log('Unknown message received:', message);
           break;
         }
