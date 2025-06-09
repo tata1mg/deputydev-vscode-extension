b78f21e7d60906436fb5c51c7769a9fe90224bab
Satyam Kumar <satyam.kumar1@1mg.com>
2025-06-09 18:10:22 +0530

    Add ability to send diff application events to sidebar

--- webviews/sidebar/src/stores/chatStore.ts
+++ webviews/sidebar/src/stores/chatStore.ts
@@ -613,6 +613,8 @@ export const useChatStore = create(
                     break;
                   }
 
+                  
+
                   case 'TOOL_USE_REQUEST_START': {
                     useChatStore.setState({ showGeneratingEffect: false });
                     const toolData = event.data as {
