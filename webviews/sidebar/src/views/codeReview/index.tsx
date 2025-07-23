5f913c936e2b997a0b8c876cdc2c3c8e2221c475
Pratham verma <pratham.verma@1mg.com>
2025-07-23 16:19:32 +0530

    Websocket fix

--- webviews/sidebar/src/views/codeReview/index.tsx
+++ webviews/sidebar/src/views/codeReview/index.tsx
@@ -140,6 +140,7 @@ export default function CodeReview() {
       reviewStatus: 'IDLE',
       steps: [],
       reviewErrorMessage: '',
+      showReviewError: false,
     });
     setShowFilesToReview(true);
   };
