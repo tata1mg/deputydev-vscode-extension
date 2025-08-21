e7cf74c74c2ad7b72162f0897af8fa8b8addaf2e
Pratham verma <pratham.verma@1mg.com>
2025-08-21 14:35:54 +0530

    minor change

--- webviews/sidebar/src/views/chat/chatElements/toolChips/ToolChipSelector.tsx
+++ webviews/sidebar/src/views/chat/chatElements/toolChips/ToolChipSelector.tsx
@@ -22,117 +22,127 @@ const ToolChipSelector: React.FC<ToolProps> = ({
   terminal,
   isHistory,
 }) => {
-  if (toolRequest?.toolName === 'ask_user_input') {
-    return <AskUserInput input={toolRequest?.requestData} />;
-  } else if (toolRequest?.toolName === 'create_new_workspace') {
-    return <CreateNewWorkspace tool_id={toolUseId} status={toolRunStatus || 'pending'} />;
-  } else if (toolRequest?.toolName === 'focused_snippets_searcher') {
-    return (
-      <FocusedSnippetSearcher
-        toolRequest={toolRequest}
-        toolResponse={toolResponse}
-        toolUseId={toolUseId}
-        toolRunStatus={toolRunStatus}
-      />
-    );
-  } else if (toolRequest?.toolName === 'related_code_searcher') {
-    return (
-      <RelatedCodeSearcher
-        toolRequest={toolRequest}
-        toolResponse={toolResponse}
-        toolUseId={toolUseId}
-        toolRunStatus={toolRunStatus}
-      />
-    );
-  } else if (toolRequest?.toolName === 'file_path_searcher') {
-    return (
-      <FilePathSearcherTool
-        toolRequest={toolRequest}
-        toolResponse={toolResponse}
-        toolUseId={toolUseId}
-        toolRunStatus={toolRunStatus}
-      />
-    );
-  } else if (toolRequest?.toolName === 'grep_search') {
-    return (
-      <GrepSearchTool
-        toolRequest={toolRequest}
-        toolResponse={toolResponse}
-        toolUseId={toolUseId}
-        toolRunStatus={toolRunStatus}
-      />
-    );
-  } else if (toolRequest?.toolName === 'web_search') {
-    return (
-      <WebSearchTool
-        toolRequest={toolRequest}
-        toolResponse={toolResponse}
-        toolUseId={toolUseId}
-        toolRunStatus={toolRunStatus}
-      />
-    );
-  } else if (toolRequest?.toolName === 'public_url_content_reader') {
-    return (
-      <PublicUrlContentReaderTool
-        toolRequest={toolRequest}
-        toolResponse={toolResponse}
-        toolUseId={toolUseId}
-        toolRunStatus={toolRunStatus}
-      />
-    );
-  } else if (toolRequest?.toolName === 'iterative_file_reader') {
-    return (
-      <IterativeFileReader
-        toolRequest={toolRequest}
-        toolResponse={toolResponse}
-        toolUseId={toolUseId}
-        toolRunStatus={toolRunStatus}
-      />
-    );
-  } else if (toolRequest?.toolName === 'execute_command') {
-    if (isHistory) {
+  switch (toolRequest?.toolName) {
+    case 'ask_user_input':
+      return <AskUserInput input={toolRequest?.requestData} />;
+
+    case 'create_new_workspace':
+      return <CreateNewWorkspace tool_id={toolUseId} status={toolRunStatus || 'pending'} />;
+
+    case 'focused_snippets_searcher':
       return (
-        <TerminalPanelHistory
+        <FocusedSnippetSearcher
           toolRequest={toolRequest}
           toolResponse={toolResponse}
           toolUseId={toolUseId}
           toolRunStatus={toolRunStatus}
         />
       );
-    }
-    return (
-      <TerminalPanel
-        tool_id={toolUseId}
-        terminal_command={(toolRequest?.requestData as string) || ''}
-        status={toolRunStatus}
-        show_approval_options={terminal?.terminal_approval_required}
-        is_execa_process={terminal?.is_execa_process || false}
-        process_id={terminal?.process_id}
-        terminal_output={terminal?.terminal_output || ''}
-        exit_code={terminal?.exit_code}
-      />
-    );
-  } else if (
-    toolRequest?.toolName === 'replace_in_file' ||
-    toolRequest?.toolName === 'write_to_file'
-  ) {
-    return (
-      <FileEditedChip
-        toolRequest={toolRequest}
-        toolResponse={toolResponse}
-        toolUseId={toolUseId}
-        toolRunStatus={toolRunStatus}
-      />
-    );
-  } else {
-    return (
-      <MCPTool
-        toolRequest={toolRequest}
-        toolResponse={toolResponse}
-        toolUseId={toolUseId}
-        toolRunStatus={toolRunStatus}
-      />
-    );
+
+    case 'related_code_searcher':
+      return (
+        <RelatedCodeSearcher
+          toolRequest={toolRequest}
+          toolResponse={toolResponse}
+          toolUseId={toolUseId}
+          toolRunStatus={toolRunStatus}
+        />
+      );
+
+    case 'file_path_searcher':
+      return (
+        <FilePathSearcherTool
+          toolRequest={toolRequest}
+          toolResponse={toolResponse}
+          toolUseId={toolUseId}
+          toolRunStatus={toolRunStatus}
+        />
+      );
+
+    case 'grep_search':
+      return (
+        <GrepSearchTool
+          toolRequest={toolRequest}
+          toolResponse={toolResponse}
+          toolUseId={toolUseId}
+          toolRunStatus={toolRunStatus}
+        />
+      );
+
+    case 'web_search':
+      return (
+        <WebSearchTool
+          toolRequest={toolRequest}
+          toolResponse={toolResponse}
+          toolUseId={toolUseId}
+          toolRunStatus={toolRunStatus}
+        />
+      );
+
+    case 'public_url_content_reader':
+      return (
+        <PublicUrlContentReaderTool
+          toolRequest={toolRequest}
+          toolResponse={toolResponse}
+          toolUseId={toolUseId}
+          toolRunStatus={toolRunStatus}
+        />
+      );
+
+    case 'iterative_file_reader':
+      return (
+        <IterativeFileReader
+          toolRequest={toolRequest}
+          toolResponse={toolResponse}
+          toolUseId={toolUseId}
+          toolRunStatus={toolRunStatus}
+        />
+      );
+
+    case 'execute_command':
+      if (isHistory) {
+        return (
+          <TerminalPanelHistory
+            toolRequest={toolRequest}
+            toolResponse={toolResponse}
+            toolUseId={toolUseId}
+            toolRunStatus={toolRunStatus}
+          />
+        );
+      }
+      return (
+        <TerminalPanel
+          tool_id={toolUseId}
+          terminal_command={(toolRequest?.requestData as string) || ''}
+          status={toolRunStatus}
+          show_approval_options={terminal?.terminal_approval_required}
+          is_execa_process={terminal?.is_execa_process || false}
+          process_id={terminal?.process_id}
+          terminal_output={terminal?.terminal_output || ''}
+          exit_code={terminal?.exit_code}
+        />
+      );
+
+    case 'replace_in_file':
+    case 'write_to_file':
+      return (
+        <FileEditedChip
+          toolRequest={toolRequest}
+          toolResponse={toolResponse}
+          toolUseId={toolUseId}
+          toolRunStatus={toolRunStatus}
+        />
+      );
+
+    default:
+      return (
+        <MCPTool
+          toolRequest={toolRequest}
+          toolResponse={toolResponse}
+          toolUseId={toolUseId}
+          toolRunStatus={toolRunStatus}
+        />
+      );
   }
 };
 
